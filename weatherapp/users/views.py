3019165bd5d33c35c039943bbcb740f0cf3df69d
Vladislav Luchnikov <whatislov@mail.ru>
2024-10-30 23:49:01 +0300

    feat: add registration, login and logout

--- weatherapp/users/views.py
+++ weatherapp/users/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
-    register_form = UserCreationForm(request.POST)
     if request.method == 'POST':
+        register_form = UserCreationForm(request.POST)
         if register_form.is_valid():
-            print('Valid!')
             register_form.save()
-            return redirect('login/')
+            return redirect('login')
+    else:
+        register_form = UserCreationForm()
     
-    return render(request, 'users/register.html', {'form': register_form})
+    return render(request, 'registration/register.html', {'form': register_form})
 
 
