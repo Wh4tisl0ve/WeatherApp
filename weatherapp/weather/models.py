8c593d361ac3e486cc6b401d5a3d4cced3a21b44
Vladislav Luchnikov <whatislov@mail.ru>
2024-10-30 23:50:03 +0300

    feat: add templates for main page

--- weatherapp/weather/models.py
+++ weatherapp/weather/models.py
@@ -1,16 +1,8 @@
 from django.db import models
-
-
-class Users(models.Model):
-    login = models.CharField(max_length=40)
-    password = models.CharField(max_length=70)
-
-    def __str__(self):
-        return f"Users(login: {self.login}, password: {self.password})"
-
+from django.contrib.auth.models import User
 
 class Locations(models.Model):
-    users = models.ForeignKey(Users, on_delete=models.CASCADE)
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
     latitude = models.DecimalField(decimal_places=10, max_digits=10)
     longitude = models.DecimalField(decimal_places=10, max_digits=10)
--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required(redirect_field_name=None)
+def main_page(request):
+    return render(request, 'weather/index.html')
