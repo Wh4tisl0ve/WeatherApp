998b516b402533ffcb15824e98a8792fd15113fc
Vladislav Luchnikov <whatislov@mail.ru>
2024-10-31 15:29:20 +0300

    feat: added logic for work with api

--- weatherapp/weather/urls.py
+++ weatherapp/weather/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import main_page
+from .views import main_page, search_page
 
 urlpatterns = [
     path("", main_page, name='main'),
+    path("search/", search_page, name='search'),
 ]
--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from .models import Locations
+from .services import get_all_locations_by_name
+
 
 
 @login_required(redirect_field_name=None)
 def main_page(request):
-    return render(request, 'weather/index.html')
+    current_user = request.user
+    id_user = current_user.id
+
+    user_locations_coord = Locations.objects.filter(users_id=id_user)
+
+    return render(request, "weather/index.html")
+
+
+@login_required(redirect_field_name=None)
+def search_page(request):
+    if request.method == "GET":
+        params = request.GET
+        location_name = params.get("name")
+
+        if location_name:
+            get_all_locations_by_name(location_name)
+
+        return render(
+            request, "weather/search.html", context={"location_name": location_name}
+        )
