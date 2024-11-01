e6e13524fe96d8f425c093ec5c88aaeab01d5232
Vladislav Luchnikov <whatislov@mail.ru>
2024-11-02 00:32:26 +0300

    refactor: update field model location

--- weatherapp/weather/models.py
+++ weatherapp/weather/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 class Locations(models.Model):
     users = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
-    latitude = models.DecimalField(decimal_places=10, max_digits=10)
-    longitude = models.DecimalField(decimal_places=10, max_digits=10)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
 
     def __str__(self):
         return f"Locations(name: {self.name}, latitude: {self.latitude}), longitude: {self.longitude}"
