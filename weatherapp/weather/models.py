a9bf9adefb7014002f906eb1f3a01093499cf11c
Vladislav Luchnikov <whatislov@mail.ru>
2024-11-07 16:47:22 +0300

    refactor: update weather app

--- weatherapp/weather/models.py
+++ weatherapp/weather/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Locations(models.Model):
-    users = models.ForeignKey(User, on_delete=models.CASCADE)
+    users = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
