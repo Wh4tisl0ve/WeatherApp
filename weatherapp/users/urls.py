f3012899d3b0445372e13dd9d4e509667fbd8b67
Vladislav Luchnikov <whatislov@mail.ru>
2024-11-09 18:17:05 +0300

    refactor: refactor users app

--- weatherapp/users/urls.py
+++ weatherapp/users/urls.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import RegistrationView, AuthorizationView
+from .views import RegistrationPageView, AuthorizationPageView
 
 urlpatterns = [
-    path('register/', RegistrationView.as_view(), name='register'),
-    path('login/', AuthorizationView.as_view(), name='login'),
+    path('register/', RegistrationPageView.as_view(), name='register'),
+    path('login/', AuthorizationPageView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
--- weatherapp/users/views.py
+++ weatherapp/users/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views.generic import TemplateView
 
 from .forms import UserCreationForm, UserAuthorizationForm
 
 
-class RegistrationView(TemplateView):
+class RegistrationPageView(TemplateView):
     def get(self, request, *args, **kwargs):
         register_form = UserCreationForm()
         return render(request, "users/register.html", {"form": register_form})
@@ -16,12 +17,12 @@ class RegistrationView(TemplateView):
             new_user = register_form.save(commit=False)
             new_user.set_password(register_form.cleaned_data["password1"])
             new_user.save()
-            return redirect("main")
+            return redirect(reverse('users:login'))
 
         return render(request, "users/register.html", {"form": register_form})
 
 
-class AuthorizationView(LoginView):
+class AuthorizationPageView(LoginView):
     authentication_form = UserAuthorizationForm
     template_name = 'users/login.html'
     redirect_authenticated_user = 'main'
