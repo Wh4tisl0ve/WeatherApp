6e1fd6bb2df4d309c7b852603a07bc352e2088d3
Vladislav Luchnikov <whatislov@mail.ru>
2024-11-21 21:35:11 +0300

    feat: add errors handling

--- weatherapp/app/urls.py
+++ weatherapp/app/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from weather.views.errors import BadRequestErrorView, ForbiddenErrorView, NotFoundErrorView, ServerErrorView
+
+handler400 = BadRequestErrorView()
+handler403 = ForbiddenErrorView()
+handler404 = NotFoundErrorView()
+handler500 = ServerErrorView()
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include(('users.urls', 'users'), namespace='accounts')),
