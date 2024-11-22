3996abf702d09b485713f6843421825ae880670d
Vladislav Luchnikov <whatislov@mail.ru>
2024-11-22 15:32:48 +0300

    feat: add handling static files

--- weatherapp/app/settings.py
+++ weatherapp/app/settings.py
@@ -108,12 +108,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-# По какому пути можно будет найти файлы
-STATIC_ROOT = "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"  
 
-# Аналогично static файлам
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  
+]
+
 MEDIA_URL = "/media/"
-MEDIA_ROOT = "media"
+MEDIA_ROOT = BASE_DIR / "media"  
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
