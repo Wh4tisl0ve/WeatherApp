4434140372b40d859e8612a915cef1de4686636a
Vladislav Luchnikov <whatislov@mail.ru>
2024-11-25 20:56:16 +0300

    refactor: delete print

--- weatherapp/auth/backend.py
+++ weatherapp/auth/backend.py
@@ -5,21 +5,17 @@ User = get_user_model()
 
 
 class CustomAuthBackend(BaseBackend):
-    def authenticate(self, request, username=None, password=None): 
+    def authenticate(self, request, username=None, password=None):
         try:
-            print('pfhfg')
             user = User.objects.get(username=username)
             if user and user.check_password(password):
                 return user
         except User.DoesNotExist:
             return None
         return None
-        
+
     def get_user(self, user_id):
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
-        
-
-        
